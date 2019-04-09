3d15bceb4aac13fb76cfba79e1796032bb01412b
grsakea <grsakea@users.noreply.github.com>
2019-04-09 16:59:16 +0200

    Add step 1

---
+++ step_1/example.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create gin application
+	router := setupRouter()
+	// Launch the application
+	err := router.Run()
+
+	fmt.Println(err)
+}
+
+func setupRouter() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	router.GET("/ping/:message", pingMessageController)
+
+	return router
+}
+
+func pingMessageController(c *gin.Context) {
+	user := c.Params.ByName("message")
+
+	c.JSON(200, gin.H{
+		"message": "hello " + user,
+	})
+}
